7d79d0ea3a416551563db98b43f7f1dceae30f41
codemaster-stack <tegaabinuwa@gmail.com>
2026-03-24 12:50:47 +0100

    updated url

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const app = express();
 // ── CORS ─────────────────────────────────────────────────
 // Allow requests from your frontend URL
 const allowedOrigins = [
-  "www.angeluni-salltd.com",
+  "https://www.angeluni-salltd.com",
   process.env.FRONTEND_URL,
   "http://localhost:3000",
   "http://localhost:5500",
